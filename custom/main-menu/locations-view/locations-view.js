c9f27a028d2f4395171a66b2a1fe4b5427e3a272
Salvador Lopez <salvador.lopez@enterconecta.com>
2019-11-25 11:14:33 -0600

    Mostrar diferentes iconos en marcadores dependiendo el valor de Estado de la Cuenta

--- custom/main-menu/locations-view/locations-view.js
+++ custom/main-menu/locations-view/locations-view.js
@@ -79,56 +79,92 @@ let LocationsView = customization.extend(NomadView, {
 
                     var mapDiv = document.getElementById("map");
 
-                     var map = plugin.google.maps.Map.getMap(mapDiv,{
+                    /*
+                    var map = plugin.google.maps.Map.getMap(mapDiv,{
                         'camera': {
                             'latLng': {"lat": 19.4326018, "lng": -99.13320490000001},
+                            'zoom': 7
+                            }
+                    });*/
+
+                    var map=plugin.google.maps.Map.getMap(mapDiv,{
+                        'camera': {
                             'zoom': 7
                             }
                         });
 
                     if(data.records.length>0){
+                        // Add markers
+                        var bounds = [];
                         for(var i=0;i<data.records.length;i++){
 
                             if(data.records[i].gps_latitud_c !="" && data.records[i].gps_longitud_c != ""){
-                            
-                                map.addMarker({
-                                    'position': {"lat":data.records[i].gps_latitud_c,"lng":data.records[i].gps_longitud_c},
-                                    'title': data.records[i].id
-                                }, function(marker) {
-                                // Show the infoWindow
-                                marker.showInfoWindow();
-                                });
-                            }
-                        }
-                    }
 
-                    },
-                    error: er => {
+                                 var icono='';
+                                 switch(data.records[i].visit_status_c){
+                                    
+                                    case "Planificado":
+                                        icono = "img/icon-negro_.png";
+                                    break;
+                                    
+                                    case "Realizado":
+                                        icono = "img/icon-verde_.png";
+                                    break;
+                                    
+                                    case "Reprogramado":
+                                        icono = "img/icon-azul_.png";
+                                    break;
+                                    case "Cancelado":
+                                        icono = "img/icon-rojo_.png";
+                                    break;
+
+                                    case "":
+                                        icono = "img/icon-negro_.png";
+                                    break;
+
+                                    default:
+                                        icono = "img/icon-negro_.png";
+                                }//switch
+                                
+                                bounds.push({"lat":data.records[i].gps_latitud_c,"lng":data.records[i].gps_longitud_c});
+
+                                var marker = map.addMarker({
+                                    'position': {"lat":data.records[i].gps_latitud_c,"lng":data.records[i].gps_longitud_c},
+                                    'title': data.records[i].id,
+                                    'icon': {
+                                        'url': icono
+                                    }
+                                }, function(marker) {
+                                    // Show the infoWindow
+                                    marker.showInfoWindow();
+                                });
+                            }//if lat lng
+                        }//for
+
+                        // Set a camera position that includes all markers.
+                        map.moveCamera({
+                            target: bounds
+                        });
+                    }//if length data.records
+                },//end success api call
+                error: er => {
                         app.alert.show('api_carga_error', {
                             level: 'error',
                             autoClose: true,
                             messages: 'Error al cargar datos: '+er,
                         });
-                    },
-                    complete: () => {
-
+                },
+                complete: () => {
                         app.alert.dismiss('accounts_load');
-                        
-                    },
-            });
-
-    },
+                },
+            });//App.api.call
+    },//end getCuentas
 
     onAfterRender(){
         var mapDiv = document.getElementById("map");
 
         // Initialize the map plugin
-        var map = plugin.google.maps.Map.getMap(mapDiv, {
-            'camera': {
-                'latLng': {"lat": 19.4326018, "lng": -99.13320490000001},
-                'zoom': 7
-                }
-            });
+        var map = plugin.google.maps.Map.getMap(mapDiv);
 
         // You have to wait the MAP_READY event.
         map.one(plugin.google.maps.event.MAP_READY, this.onMapInit);
@@ -137,6 +173,7 @@ let LocationsView = customization.extend(NomadView, {
     onMapInit(map) {
 
         // Add a marker
+        /*
         map.addMarker({
             'position': {"lat": 19.4326018, "lng": -99.13320490000001},
             'title': "MARKER ESTATICO!"
@@ -144,6 +181,7 @@ let LocationsView = customization.extend(NomadView, {
             // Show the infoWindow
             marker.showInfoWindow();
         });
+        */
 
     }
 });
